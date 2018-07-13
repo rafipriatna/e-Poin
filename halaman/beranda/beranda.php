054fa0c07e3f35f16caba719a5a66e69e4a5984f
Rafi Priatna <stormrider136@gmail.com>
2018-07-13 13:17:19 +0700

    Mengganti ucapan selamat datang di beranda

--- halaman/beranda/beranda.php
+++ halaman/beranda/beranda.php
@@ -1,1 +1,21 @@
-Selamat datang di halaman beranda.
+<div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Beranda</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active">Beranda</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="content mt-3">
+            안녕하세요 <?php echo $data['nama_pengguna'];?>
+            </div>
