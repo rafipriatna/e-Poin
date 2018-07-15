c775c296e5c721379a109ec63f742cc6ea81f43b
Rafi Priatna <stormrider136@gmail.com>
2018-07-15 22:27:57 +0700

    Mengaktifkan fungsi ctrl + v

--- tata_letak/bawah.php
+++ tata_letak/bawah.php
@@ -26,7 +26,7 @@ $(document).ready(function() {
     // Hanya untuk non numeric untuk input angka.
     $("input[type=number]").keydown(function (e) {
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
-            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+            ((e.keyCode == 65 || e.keyCode == 86 || e.keyCode == 67) && (e.ctrlKey === true || e.metaKey === true)) ||
             (e.keyCode >= 35 && e.keyCode <= 40)) {
                  return;
         }
