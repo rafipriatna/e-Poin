354d39cb04d4b0236d8db6cf4d56df24a07d4ce0
adrianardizza <adrian.ardizza@gmail.com>
2018-10-21 05:35:22 +0700

    Fixed margin styling that caused the content to overreach page limit.

--- halaman/beranda/beranda.php
+++ halaman/beranda/beranda.php
@@ -17,16 +17,16 @@
             </div>
         </div>
 
-            <div class="content m-5">
-              <div class="row">
+            <div class="content m-6">
+              <div class="row m-4">
                 <div class="col-sm-12">
                   <h2>Selamat datang, <?php echo $data['nama_pengguna'];?>.</h2>
                   <h3>Hari ini adalah tanggal <?php echo date("d/m/Y") ?> </h3>
                 </div>
               </div>
 
-              <div class="row mt-4">
-                  <div class="col-md-4">
+              <div class="row m-4">
+                  <div class="col-md-6">
                         <div class="card">
                           <div class="card-body">
                             <h5 class="card-title">Piket</h5>
@@ -36,7 +36,7 @@
                         </div>
                   </div>
 
-                  <div class="col-md-4">
+                  <div class="col-md-6">
                         <div class="card">
                           <div class="card-body">
                             <h5 class="card-title">Test Title</h5>
@@ -45,17 +45,9 @@
                         </div>
                 </div>
 
-                <div class="col-md-4">
-                      <div class="card">
-                        <div class="card-body">
-                          <h5 class="card-title">Test Title</h5>
-                          <p class="card-text">Test text for feature cards.</p>
-                        </div>
-                      </div>
-              </div>
             </div>
-
-            <div class="row mt-4">
+            <div class="content m-6">
+            <div class="row">
               <div class="col-lg-12">
                   <div class="card">
                       <div class="card-header">
@@ -99,4 +91,5 @@
                   </div>
               </div>
             </div>
+          </div>
   </div>
