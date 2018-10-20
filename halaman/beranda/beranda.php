bd974c21339fb39911324aa27908cfa9895dde20
adrianardizza <adrian.ardizza@gmail.com>
2018-10-21 04:24:31 +0700

    Temporary layout for new home page.

--- halaman/beranda/beranda.php
+++ halaman/beranda/beranda.php
@@ -16,6 +16,42 @@
                 </div>
             </div>
         </div>
-            <div class="content mt-3">
-            안녕하세요 <?php echo $data['nama_pengguna'];?>
+
+            <div class="content m-4">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h2>Selamat datang, <?php echo $data['nama_pengguna'];?>.</h2>
+                  <h3>Hari ini adalah tanggal <?php echo date("d/m/Y") ?> </h3>
+                  <h4>Silahkan pilih page yang anda butuhkan.</h4>
+                </div>
+              </div>
+
+              <div class="row mt-4">
+                  <div class="col-md-4">
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">Test Title</h5>
+                            <p class="card-text">Test text for feature cards.</p>
+                          </div>
+                        </div>
+                  </div>
+
+                  <div class="col-md-4">
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">Test Title</h5>
+                            <p class="card-text">Test text for feature cards.</p>
+                          </div>
+                        </div>
+                </div>
+
+                <div class="col-md-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <h5 class="card-title">Test Title</h5>
+                          <p class="card-text">Test text for feature cards.</p>
+                        </div>
+                      </div>
+              </div>
             </div>
+  </div>
